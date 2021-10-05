d758a124cc3b1f45ef3b7199d84470b816f78438
willh003 <willh003@gmail.com>
2021-10-05 18:09:56 -0400

    Initial Commit

---
+++ MoneyWatch/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MoneyWatch
+//
+//  Created by Will H on 10/5/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
